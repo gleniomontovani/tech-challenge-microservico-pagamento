3ee41ba69bb2d6baaaa9611cb1a0050b2bfb666a
gleniomontovani <gleniomontovani@gmail.com>
2024-04-04 11:00:44 -0300

    Commit inicial

---
+++ src/main/java/br/com/postech/techchallenge/microservico/pagamento/TechchallengeMicroservicoPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallenge.microservico.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeMicroservicoPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallengeMicroservicoPagamentoApplication.class, args);
+	}
+
+}
